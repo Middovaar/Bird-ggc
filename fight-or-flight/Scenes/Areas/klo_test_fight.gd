ca9830013f4149d692dbb381c8156c871ec8bfdd
Tolz <presamo.9@gmail.com>
2026-02-19 18:14:02 +0100

    OLDupdate
    Pushing an old Update to clean out the repo and solve some missing files

--- fight-or-flight/Scenes/Areas/klo_test_fight.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready():
-	Global.klo_patrol_1 = $klo_patrol_1.global_position
-	Global.klo_patrol_2 = $klo_patrol_2.global_position
--- fight-or-flight/global.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-#init player
-var player_dmg_zone: Area2D
-var player_body: Area2D
-
-#init klo
-var klo_patrol_1: Vector2
-var klo_patrol_2: Vector2
